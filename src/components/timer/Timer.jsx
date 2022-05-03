92e1b0e77b199049200538049aa80a78fae361c0
denrique-alvarez <denrique.alvarez@gmail.com>
2022-05-03 19:47:15 +0200

    double digits

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,8 +1,11 @@
 export const Timer = ({ hours, minutes, seconds, status }) => {
   return (
     <div>
-      <h1>{ hours } : { minutes } : { seconds }</h1>
+      <h1>{ twoDigits(hours) } : { twoDigits(minutes) } : { twoDigits(seconds) }</h1>
       <h2>Status: { status }</h2>
     </div>
   )
 }
+
+// https://stackoverflow.com/a/2998874/1673761
+const twoDigits = (num) => String(num).padStart(2, '0');
